494383333714036431abcc376c9f784d9d2225e1
gabrielpereira1603 <gabriel_ap02@yahoo.com>
2023-07-27 16:54:45 -0300

    gerenciando rotas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
+Route::get('contato', 'ContatoController@contato')->name('site.contato');
+// Route::get($uri, $callback) 
+
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+//AGRUPAR ROUTAR COM O PREFIXO /APP 
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');    
+});
+
+Route::get('/rota1', function() {
+    echo 'rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+Route::fallback(function() {
+     echo 'A rota acessada nao existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a pagina principal';
 });
